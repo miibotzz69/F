502511297e33cb3f4618cc7084edea89278572d3
miibotzz69 <ciiminyak@gmail.com>
2025-06-23 14:05:27 +0700

    Add files via upload

---
+++ AiXsetting.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: [6673505038], // ganti pake id tele mu
+    botToken: "7577873915:AAEqAJNyJlgjBJ_fQr_zSmT2VPCtxxi5LnE", // ganti tokem bot mu
+}
+
+module.exports = global;
